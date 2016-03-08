87b085bba020fffbc8dc3c0fe6bb74454f27daa1
TonyStrafford <TonyStrafford@users.noreply.github.com>
2016-03-08 12:04:21 +0000

    Setting Up ASP.NET Identity

---
+++ Identity/Infrastructure/AppUserManager.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Identity.Models;
+
+
+namespace Identity.Infrastructure
+{
+    public class AppUserManager : UserManager<AppUser>
+    {
+        public AppUserManager(IUserStore<AppUser> store) : base(store)
+        {
+        }
+
+        // Called when Identity needs an instance of the AppUserManager
+        // The IOwinContext implementation passed as an argument defines a generically typed Get method
+        // that returns instances of objects that have been registered in the OWIN start class.
+        public static AppUserManager Create(IdentityFactoryOptions<AppUserManager> options, IOwinContext context)
+        {
+            AppIdentityDbContext db = context.Get<AppIdentityDbContext>();
+            AppUserManager manager = new AppUserManager(new UserStore<AppUser>(db));
+
+            return manager;
+        }
+    }
+}
