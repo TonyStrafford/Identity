b7a090d75c0a32ca5576dd9260fdd7bd21ea3542
TonyStrafford <TonyStrafford@users.noreply.github.com>
2016-03-08 11:30:25 +0000

    Created user class and configured Database Context class

---
+++ Identity/Infrastructure/AppIdentityDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Identity.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Identity.Infrastructure
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        // Pass the connection string to the ASP.NET Identity base class
+        public AppIdentityDbContext() : base("IdentityDB")
+        {
+        }
+
+        static AppIdentityDbContext()
+        {
+            Database.SetInitializer<AppIdentityDbContext>(new IdentityDbInit());
+        }
+
+        // This is how instances of the class will be created when needed by OWIN
+        public static AppIdentityDbContext Create()
+        {
+            return new AppIdentityDbContext();
+        }
+    }
+
+    public class IdentityDbInit : DropCreateDatabaseIfModelChanges<AppIdentityDbContext>
+    {
+        protected override void Seed(AppIdentityDbContext context)
+        {
+            PerformInitialSetup(context);
+            base.Seed(context);
+        }
+
+        public void PerformInitialSetup(AppIdentityDbContext context)
+        {
+            // initial configuration will go here
+        }
+    }
+}
