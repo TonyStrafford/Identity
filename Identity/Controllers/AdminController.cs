f1645c891f7427b86cbd2c07c6f129a8becbf73c
TonyStrafford <TonyStrafford@users.noreply.github.com>
2016-03-08 19:42:16 +0000

    Able to create users

---
+++ Identity/Controllers/AdminController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using Identity.Infrastructure;
+using Identity.Models;
+using Microsoft.AspNet.Identity;
+
+namespace Identity.Controllers
+{
+    public class AdminController : Controller
+    {
+        private AppUserManager UserManager => HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+
+        public ActionResult Index()
+        {
+            // The AppUserManager.Users property returns an enumeration of user objects — instances of the AppUser class in our application
+            // which can be queried and manipulated using LINQ
+            return View(UserManager.Users);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser { UserName = model.Name, Email = model.Email };
+                IdentityResult result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else {
+                    AddErrorsFromResult(result);
+                }
+            }
+            return View(model);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (string error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+    }
+}
