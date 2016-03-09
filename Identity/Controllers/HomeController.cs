bd648b9e98c375813e751d884e6856481d123887
TonyStrafford <TonyStrafford@users.noreply.github.com>
2016-03-09 10:46:51 +0000

    Authenticating Users

--- Identity/Controllers/HomeController.cs
+++ Identity/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace Identity.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             Dictionary<string, object> data = new Dictionary<string, object>();
--- Identity/Models/UserViewModels.cs
+++ Identity/Models/UserViewModels.cs
@@ -16,4 +16,12 @@ namespace Identity.Models
         [Required]
         public string Password { get; set; }
     }
+
+    public class LoginModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
 }
